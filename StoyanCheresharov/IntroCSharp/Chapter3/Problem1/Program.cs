c160fb4033f13311c501269a041e3092a3da99fa
wingman007 <cheresharov@hotmail.com>
2019-02-02 12:48:17 +0200

    Chapter 3 Problem 1 Solved

---
+++ StoyanCheresharov/IntroCSharp/Chapter3/Problem1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Напишете израз, който да проверява дали дадено цяло число е четно или нечетно.
+        int n = 5;
+        Console.WriteLine((n % 2 == 1)? "odd" : "even");
+    }
+}
+
