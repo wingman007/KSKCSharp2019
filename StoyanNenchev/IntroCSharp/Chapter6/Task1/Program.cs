7e42d168c3a802129b2918ae349281186073967b
kaaremass <kaaremasmain@gmail.com>
2019-02-22 11:32:23 +0200

    New solutins

---
+++ StoyanNenchev/IntroCSharp/Chapter6/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task1
+{
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            //Напишете програма, която отпечатва на конзолата числата от 1 до N. Числото N трябва да се чете от стандартния вход.
+
+
+            int n = 0;
+            do
+            {
+                Console.Write("Please enter a positive number:");
+                n = int.Parse(Console.ReadLine());
+
+            } while (n < 1);
+
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ StoyanNenchev/IntroCSharp/Chapter6/Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Напишете програма, която чете от конзолата поредица от цели числа и отпечатва най-малкото и най-голямото от тях.
+
+
+            do
+            {
+                Console.Write("Please enter a positive number:");
+                int n = int.Parse(Console.ReadLine());
+
+            } while (n < 1);
+
+
+            int smallest = int.MinValue;
+            int biggest = int.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                if (smallest > n)
+                {
+                    smallest = n;
+                }
+                if (biggest < n)
+                {
+                    biggest = n;
+                }
+
+
+            }
+            Console.WriteLine(smallest);
+            Console.WriteLine(biggest);
+        }
+
+    }
+}
